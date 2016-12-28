d5571fecaf3e5c7cc246d266cc45fa5c7d70c419
Todd Kennedy <toddself@users.noreply.github.com>
2016-12-28 14:17:31 -0800

    Total Rewrite (#1)
    Use the React API better to provide a wrapper for the components, and propagate
    the changes down into the correctly.

--- test/test.js
+++ test/test.js
@@ -1,88 +1,86 @@
 const tape = require('tape')
-const br = require('../')
+const React = require('react')
+require('react-addons-test-utils')
+const {mount} = require('enzyme')
+const bearact = require('../')
+let count = 0
+
+const testStore = {
+  namespace: 'foo',
+  state: {
+    header: 'test'
+  },
+  reducers: {
+    update: function (state, data) {
+      ++count
+      return {header: data}
+    }
+  },
+  effects: {
+    makeError: function (state, data, send, done) {
+      done(new Error('boop'))
+    }
+  }
+}
+
+const TestComponent = React.createClass({
+  change: function (evt) {
+    evt.preventDefault()
+    this.props.send('update', 'foobar')
+  },
+  render: function () {
+    const props = {
+      onClick: (evt) => this.props.send('update', 'foobar'),
+      onBlur: (evt) => this.props.send('makeError')
+    }
+    return React.createElement('h1', props, `${this.props.err || this.props.model.header}`)
+  }
+})
 
 tape('basic', (t) => {
-  t.plan(1)
-  const store = br()
-  const model = {
-    namespace: 'test',
-    state: {
-      val: false
-    },
-    reducers: {
-      setVal: (data, state) => ({val: true})
-    }
-  }
-  const view = {
-    setState: (state) => {
-      t.ok(state.val)
-    }
-  }
-  const send = store.register(model, view)
-  send('test:setVal')
+  t.plan(4)
+  const Test = bearact(TestComponent, testStore)
+  const el = React.createElement(Test, {}, null)
+  const wrapper = mount(el)
+  const h1 = wrapper.find('h1')
+  t.equal(h1.text(), testStore.state.header)
+  h1.simulate('click')
+  setTimeout(() => {
+    const h1 = wrapper.find('h1')
+    t.equal(h1.text(), 'foobar')
+    h1.simulate('blur')
+    setTimeout(() => {
+      const h1 = wrapper.find('h1')
+      t.equal(h1.text(), 'Error: boop')
+      wrapper.unmount()
+      h1.simulate('click')
+      setTimeout(() => {
+        t.equal(count, 1)
+      }, 5)
+    }, 5)
+  }, 5)
 })
 
-tape('dereg', (t) => {
-  t.plan(1)
-  const store = br()
-  const model = {
-    namespace: 'test',
+tape('no namespace', (t) => {
+  const store = {
     state: {
-      val: false
+      header: 'beep'
     },
     reducers: {
-      setVal: (data, state) => ({val: true})
+      update: function (state, data) {
+        return {header: data}
+      }
     }
   }
-  const view = {
-    setState: (state) => {
-      t.ok(state.val)
-    }
-  }
-  const send = store.register(model, view)
-  store.deregister(model)
-  send('test:setVal')
-  t.ok(true)
+  const Test = bearact(TestComponent, store)
+  const el = React.createElement(Test, {}, null)
+  const wrapper = mount(el)
+  const h1 = wrapper.find('h1')
+  t.equal(h1.text(), store.state.header)
+  h1.simulate('click')
+  setTimeout(() => {
+    t.equal(wrapper.find('h1').text(), 'foobar')
+    t.end()
+  }, 5)
 })
 
-tape('dereg string', (t) => {
-  t.plan(1)
-  const store = br()
-  const model = {
-    namespace: 'test',
-    state: {
-      val: false
-    },
-    reducers: {
-      setVal: (data, state) => ({val: true})
-    }
-  }
-  const view = {
-    setState: (state) => {
-      t.ok(state.val)
-    }
-  }
-  const send = store.register(model, view)
-  store.deregister('test')
-  send('test:setVal')
-  t.ok(true)
-})
-
-tape('throws with no namespace', (t) => {
-  t.plan(1)
-  const store = br()
-  const model = {
-    state: {
-      val: false
-    },
-    reducers: {
-      setVal: (data, state) => ({val: true})
-    }
-  }
-  const view = {
-    setState: (state) => {
-      t.ok(state.val)
-    }
-  }
-  t.throws(() => store.register(model, view))
-})
